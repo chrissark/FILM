bf4c0eaab21506d829d9d592674a00f05e26e94d
chrissark <sarkisyan.kv@phystech.edu>
2022-06-11 00:32:51 +0300

    placeholders are added if number of episodes is less than split size

--- utils/leaderboard_script.py
+++ utils/leaderboard_script.py
@@ -7,15 +7,18 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dn_startswith', type=str, required=True)
+parser.add_argument('--scene_names_path', type=str, required=True)
 #parser.add_argument('--seen', action='store_true')
 parser.add_argument('--json_name', type=str)
+parser.add_argument('--scene_descs_path', type=str, required=True)
 
 args = parser.parse_args()
+
 
 if args.json_name is None:
 	args.json_name = args.dn
 
-# returns 'seen' or 'unseen' logs
+# returns 'seen' or 'unseen' logs, all scene names
 def return_logs(split):
 	pickle_globs = glob("results/leaderboard/actseqs_test_" + split + "_" + args.dn_startswith + "*")
 	pickles = []
@@ -40,10 +43,36 @@ def return_logs(split):
 				new_actions.append(action)
 		total_logs[i] = {key: new_actions}
 
-	return total_logs
+   #scene names (tasks id)
+	scene_names_json = json.load(open(args.scene_names_path + '/oct21.json', 'r'))
+	scene_names = []
+	split_data = scene_names_json['tests_' + split]
+	for e in split_data:
+		r_idx = e['repeat_idx']
+		task = e['task']
+		path_to_json = args.scene_descs_path + f'/{task}/pp/ann_{r_idx}.json'
+		traj_data = json.load(open(path_to_json, 'r'))
+		task_id = traj_data['task_id']
+		scene_names.append(task_id)
 
-total_logs_seen = return_logs('seen')
-total_logs_unseen = return_logs('unseen')
+	print(f'Num scene names: {len(scene_names)}')
+    
+
+	return total_logs, scene_names
+
+total_logs_seen, scene_names_seen = return_logs('seen')
+total_logs_unseen, scene_names_unseen = return_logs('unseen')
+
+#placeholders
+if len(total_logs_seen) < 1533:
+	print('Number of test_seen episodes is less than 1533. Adding placeholders')
+	for i in range(len(total_logs_seen), 1533):   
+		total_logs_seen.append({scene_names_seen[i]: [{'action': 'LookDown_15', 'forceAction': True}]})
+
+if len(total_logs_unseen) < 1529:
+	print('Number of test_unseen episodes is less than 1529. Adding placeholders')
+	for i in range(len(total_logs_unseen), 1529):  
+		total_logs_unseen.append({scene_names_unseen[i]: [{'action': 'LookDown_15', 'forceAction': True}]})
 
 
 results = {'test_unseen': total_logs_unseen, 'test_seen': total_logs_seen}
