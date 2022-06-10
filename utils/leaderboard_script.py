e1980ba966759f74d1d905fa400d45de03bdc334
chrissark <sarkisyan.kv@phystech.edu>
2022-06-10 20:19:19 +0300

    leaderboard_script.py updated. It creates one json-file with seen and unseen logs without length assertion, which is still in question

--- utils/leaderboard_script.py
+++ utils/leaderboard_script.py
@@ -7,7 +7,7 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dn_startswith', type=str, required=True)
-parser.add_argument('--seen', action='store_true')
+#parser.add_argument('--seen', action='store_true')
 parser.add_argument('--json_name', type=str)
 
 args = parser.parse_args()
@@ -15,40 +15,40 @@ args = parser.parse_args()
 if args.json_name is None:
 	args.json_name = args.dn
 
-seen_str = 'seen'
-if not(args.seen):
-	seen_str= 'unseen'
+# returns 'seen' or 'unseen' logs
+def return_logs(split):
+	pickle_globs = glob("results/leaderboard/actseqs_test_" + split + "_" + args.dn_startswith + "*")
+	pickles = []
+	for g in pickle_globs:
+		pickles += pickle.load(open(g, 'rb'))
 
-pickle_globs = glob("results/leaderboard/actseqs_test_" + seen_str + "_" + args.dn_startswith + "*")
+	total_logs =[]
+	for i, t in enumerate(pickles):
+		key = list(t.keys())[0]
+		actions = t[key]
+		trial = key[1]
+		total_logs.append({trial:actions})
 
-pickles = []
-for g in pickle_globs:
-	pickles += pickle.load(open(g, 'rb'))
+	for i, t in enumerate(total_logs):
+		key = list(t.keys())[0]
+		actions = t[key]
+		new_actions = []
+		for action in actions:
+			if action['action'] == 'LookDown_0' or action['action'] == 'LookUp_0':
+				pass
+			else:
+				new_actions.append(action)
+		total_logs[i] = {key: new_actions}
 
-total_logs =[]
-for i, t in enumerate(pickles):
-	key = list(t.keys())[0]
-	actions = t[key]
-	trial = key[1]
-	total_logs.append({trial:actions})
+	return total_logs
 
-for i, t in enumerate(total_logs):
-	key = list(t.keys())[0]
-	actions = t[key]
-	new_actions = []
-	for action in actions:
-		if action['action'] == 'LookDown_0' or action['action'] == 'LookUp_0':
-			pass
-		else:
-			new_actions.append(action)
-	total_logs[i] = {key: new_actions}
+total_logs_seen = return_logs('seen')
+total_logs_unseen = return_logs('unseen')
 
-if args.seen:
-	assert len(total_logs) == 1533
-	results = {'tests_unseen':[{'trial_T20190908_010002_441405': [{'action': 'LookDown_15', 'forceAction': True}]}], 'tests_seen': total_logs}
-else:
-	assert len(total_logs) == 1529
-	results = {'tests_seen':[{'trial_T20190909_042500_949430': [{'action': 'LookDown_15', 'forceAction': True}]}], 'tests_unseen': total_logs}
+
+results = {'test_unseen': total_logs_unseen, 'test_seen': total_logs_seen}
+print(f'Num seen episodes: {len(total_logs_seen)}')
+print(f'Num unseen episodes: {len(total_logs_unseen)}')     
 
 if not os.path.exists('leaderboard_jsons'):
 	os.makedirs('leaderboard_jsons')
